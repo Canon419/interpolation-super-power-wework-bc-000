1104d4dbdccfbc446ca21d74b2f7810e52404e4f
Canon Turner <canon.turner@wework.com>
2018-02-13 20:48:48 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 def display_rainbow (letter, color)
 letter = ["r", "o", "y", "g", "b", "i", "v"]
 color = ["red", "orange" "yellow", "green", "blue" "indigo" "violet"]
-puts "display_rainbow (letter , color)
+puts "display_rainbow (letter , color)"
 end
 
