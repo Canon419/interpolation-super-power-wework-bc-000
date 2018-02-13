eab717eaeaad26cd2bcf40273cffaff8afcc4854
Canon Turner <canon.turner@wework.com>
2018-02-13 21:19:10 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 def display_rainbow(array) 
 colors = ["red", "orange" "yellow", "green", "blue" "indigo" "violet"]
 letters = ["r", "o", "y", "g", "b", "i", "v"]
-  puts ""
+  puts "R: #{array[0]}, O: #{array[1]}, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end
